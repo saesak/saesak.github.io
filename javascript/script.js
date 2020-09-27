f572264dfb8a7782fe3f83854cd1c04489dc9cc5
saesak <40836287+saesak@users.noreply.github.com>
2020-09-27 22:13:23 +0900

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,30 @@
+//for the mouseover on the letters at the top
+const spans = document.querySelectorAll('h1 span')
+spans.forEach(span => span.addEventListener('mouseover', function(e) {
+  span.classList.add('animated', 'rubberBand')
+}))
+spans.forEach(span => span.adEventListener('mouseout', function(e) {
+  span.classList.remove('animated', 'rubberBand')
+}))
+
+//for the level bars
+const pythonBar = document.querySelectorAll('.bar-html')
+const reactBar = document.querySelectorAll('.bar-react')
+const mysqlBar = document.querySelectorAll('.bar-mysql')
+
+var t1 = new TimelineLite()
+
+t1.fromTo(pythonBar, 0.75, {width: 'calc(0% - 6px)'}, {width: 'calc(90% - 6px)', ease: Power4.easeOut})
+  .fromTo(reactBar, 0.75, {width: 'calc(0% - 6px)'}, {width: 'calc(90% - 6px)', ease: Power4.easeOut})
+  .fromTo(mysqlBar, 0.75, {width: 'calc(0% - 6px)'}, {width: 'calc(90% - 6px)', ease: Power4.easeOut})
+
+const controller = new ScrollMagic.Controller()
+const scene = new ScrollMagic.Scene({
+  triggerElement: '.skills',
+  triggerHook: 0
+})
+.setTween(t1)
+.addTo(controller)
+
+//didn't do the showing work category
 
