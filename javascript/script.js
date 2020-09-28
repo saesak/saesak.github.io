e21bb264411f1f36fa4309b930d21f631c195419
saesak <40836287+saesak@users.noreply.github.com>
2020-09-28 20:00:44 +0900

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -15,16 +15,16 @@ const mysqlBar = document.querySelectorAll('.bar-mysql')
 var t1 = new TimelineLite()
 
 t1.fromTo(pythonBar, 0.75, {width: '0px'}, {width: '60px', ease: Power4.easeOut})
-t1.fromTo(reactBar, 0.75, {width: 'calc(0% - 6px)'}, {width: 'calc(90% - 6px)', ease: Power4.easeOut})
-t1.fromTo(mysqlBar, 0.75, {width: 'calc(0% - 6px)'}, {width: 'calc(90% - 6px)', ease: Power4.easeOut})
+t1.fromTo(reactBar, 0.75, {width: '0px'}, {width: '300px', ease: Power4.easeOut})
+t1.fromTo(mysqlBar, 0.75, {width: '300px'}, {width: '0px', ease: Power4.easeOut})
 
 const controller = new ScrollMagic.Controller()
 const scene = new ScrollMagic.Scene({
-  triggerElement: '.skills',
-  triggerHook: 0
+  triggerElement: '#skills',
+  triggerHook: 1
 })
-scene.setTween(t1)
-scene.addTo(controller)
+  .setTween(t1)
+  .addTo(controller)
 
 //didn't do the showing work category
 
