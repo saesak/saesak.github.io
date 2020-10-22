573df626f2936ceebbd97016feb4d8efd0bb0ada
saesak <40836287+saesak@users.noreply.github.com>
2020-10-22 22:12:41 +0900

    Update slideshow.js

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -1,25 +1,31 @@
-  var slideshows = document.querySelectorAll('[data-component="slideshow"]');
-  
-  // Apply to all slideshows that you define with the markup wrote
-  slideshows.forEach(initSlideShow);
-
-  function initSlideShow(slideshow) {
-
-    var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`); // Get an array of slides
-
-    var index = 0, time = 5000;
-    slides[index].classList.add('active');  
-    
-    setInterval( () => {
-      slides[index].classList.remove('active');
-      
-      //Go over each slide incrementing the index
-      index++;
-      
-      // If you go over all slides, restart the index to show the first slide and start again
-      if (index === slides.length) index = 0; 
-      
-      slides[index].classList.add('active');
-
-    }, time);
-  }
+document.querySelector(".prevBtn").addEventListener("click", () => {
+   changeSlides(-1);
+});
+document.querySelector(".nextBtn").addEventListener("click", () => {
+   changeSlides(1);
+});
+var slideIndex = 1;
+showSlides(slideIndex);
+function changeSlides(n) {
+   showSlides((slideIndex += n));
+}
+function currentSlide(n) {
+   showSlides((slideIndex = n));
+}
+function showSlides(n) {
+   var i;
+   var slides = document.getElementsByClassName("Slide");
+   var dots = document.getElementsByClassName("Navdot");
+   if (n > slides.length) {
+      slideIndex = 1;
+   }
+   if (n < 1) {
+      slideIndex = slides.length;
+   }
+   Array.from(slides).forEach(item => (item.style.display = "none"));
+   Array.from(dots).forEach(
+      item => (item.className = item.className.replace(" selected", ""))
+   );
+   slides[slideIndex - 1].style.display = "block";
+   dots[slideIndex - 1].className += " selected";
+}
