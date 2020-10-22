441a37a4f9ed5ad4c00c27c79871809abd7bbe8d
saesak <40836287+saesak@users.noreply.github.com>
2020-10-22 20:44:43 +0900

    Update slideshow.js

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -1,21 +1,25 @@
+  var slideshows = document.querySelectorAll('[data-component="slideshow"]');
+  
+  // Apply to all slideshows that you define with the markup wrote
+  slideshows.forEach(initSlideShow);
 
-var slideshows = document.querySelectorAll('[data-component="slideshow"]');
-slideshows.forEach(initSlideShow);
+  function initSlideShow(slideshow) {
 
-function initSlideShow(slideshow) {
+    var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`); // Get an array of slides
 
-	var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`);
+    var index = 0, time = 5000;
+    slides[index].classList.add('active');  
+    
+    setInterval( () => {
+      slides[index].classList.remove('active');
+      
+      //Go over each slide incrementing the index
+      index++;
+      
+      // If you go over all slides, restart the index to show the first slide and start again
+      if (index === slides.length) index = 0; 
+      
+      slides[index].classList.add('active');
 
-	var index = 0, time = 1000;
-	slides[index].classList.add('active');
-
-	setInterval( () => {
-		slides[index].classList.remove('active');
-		
-		index++;
-		if (index === slides.length) index = 0;
-
-		slides[index].classList.add('active');
-
-	}, time);
-}
+    }, time);
+  }
