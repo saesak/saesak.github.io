da8d90905cc2436c7693c6e8b42ec41e2c5f62bf
saesak <40836287+saesak@users.noreply.github.com>
2021-01-16 20:36:23 +0900

    Update script.js
    updated for showcategory and hidecategory functions for the work section where you show images of the projects you did

--- javascript/script.js
+++ javascript/script.js
@@ -26,5 +26,28 @@ const scene = new ScrollMagic.Scene({
   .setTween(t1)
   .addTo(controller)
 
+const showRequiredCategory = event => {
+  const getId = event.id
+  const links = document.querySelectorAll('.work-category button')
+  for(i=0; i<links.length; i++) {
+    if(links[i].hasAttribute('class')) {
+      links[i].classList.remove('active')
+    }
+  }
+  
+  event.classList.add('active')
+  const getCategory = document.querySelector('.category-${getId}')
+  const categories = document.querySelectorAll('div[class ^= "category-"]')
+  for(i=0; i<categories.length; i++) {
+    if(categories[i].hasAttribute('class')) {
+      categories[i].classList.remove('showCategory')
+      categories[i].classList.add('hideCategory')
+    }
+  }
+  
+  getCategory.classList.remove('hideCategory')
+  getCategory.classList.add('showCategory')
+}
+
 //didn't do the showing work category
 
