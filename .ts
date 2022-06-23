c8ec5c1706a59523c0c8905752cb747b0a1804c3
saesak <matthewcho2000@gmail.com>
2022-06-23 03:52:38 -0400

    asdf

---
+++ .ts
@@ -0,0 +1,1 @@
+//to prevent error in tsconfig.json in svelte-kit directory
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,30 @@
 import adapter from "@sveltejs/adapter-static"; 
+
+export default {
+	kit: {
+		adapter: adapter({
+			// default options are shown
+			pages: 'docs',
+			assets: 'docs',
+			domain: null,
+			jekyll: false,
+			fallback: null,
+			precompress: false
+		}),
+		paths: {
+			base: '/saesak.github.io', //repo name
+		}
+	}
+};
+
+/* import adapter from "@sveltejs/adapter-static"; 
 // was "@sveltejs/adapter-auto"
 
 const dev = "production" === "development";
 
 /** @type {import(""@sveltejs/kit").Config} */
-const config = {
+
+/*const config = {
     kit: {
         adapter: adapter({
             pages: "docs",
@@ -14,4 +34,4 @@ const config = {
     }
 };
 
-export default config;
+export default config;*/
